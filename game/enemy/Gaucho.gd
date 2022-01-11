ddcd6ddb1e4bd30221e6b159b1903e835aa80627
manuelxp95 <manuelxp95@gmail.com>
2022-01-11 17:06:55 -0300

    Implement gaucho

--- game/enemy/Gaucho.gd
+++ game/enemy/Gaucho.gd
@@ -5,6 +5,7 @@ var direction= Vector2(1.0,0)
 var can_move = true
 
 onready var det_environment = $AnimatedSprite/Detector
+onready var det_player = $AnimatedSprite/DetectorPlayer
 onready var animation_tree= get_node("AnimationTree")
 onready var state_machine
 
@@ -14,19 +15,34 @@ func _ready():
 func _process(delta):
 	tree_control()
 
+#--------------Control of movement
 func tree_control():
-	if det_environment.is_colliding():
+	#------------------- Dont view player
+	if det_environment.is_colliding() && !det_player.is_colliding():
 		can_move= false
 		state_machine.travel("idle")
 		move_and_slide(Vector2(0,0))
+		#------------- Rotate body
 		direction = -direction
 		speed = -speed
 		det_environment.scale.x=-(det_environment.scale.x)
+		det_player.scale.x=-(det_player.scale.x)
+		$AreaHit/AttackRange.position.x= -$AreaHit/AttackRange.position.x
+		
 		yield(get_tree().create_timer(1.0),"timeout")
 		can_move=true
 		state_machine.travel("walk")
+	
+	#---------------- View the player
+	if det_player.is_colliding():
+		can_move= false
+		run(speed)
+		if det_environment.is_colliding():
+			attack()
+	else:
+		can_move=true
+	
 	walk(speed)
-		
 
 func walk(sp):
 	if can_move:
@@ -35,9 +51,18 @@ func walk(sp):
 		animation_tree.set('parameters/walk/blend_position', direction)
 		state_machine.travel("walk")
 
-func run():
-	move_and_slide(Vector2(speed*2,0))
+func run(sp):
+	animation_tree.set('parameters/run/blend_position', direction)
+	move_and_slide(Vector2(sp*1.6,0))
 	state_machine.travel("run")
 
 func attack():
+	animation_tree.set('parameters/attack/blend_position', direction)
+	move_and_slide(Vector2(0,0))
 	state_machine.travel("attack")
+
+
+func _on_AreaHit_body_entered(body):
+	if body.get_name() == "Carpincho":
+		body.collision.set_deferred("disabled",true)
+		body.damage()
