ea402909ec444c3364b4a185225e60100dc143d1
manuelxp95 <manuelxp95@gmail.com>
2022-02-02 13:11:48 -0300

    Score screen

--- game/Hud/MainMenu.gd
+++ game/Hud/MainMenu.gd
@@ -10,3 +10,12 @@ func _on_ButtonStart_pressed():
 
 func _on_ButtonExit_pressed():
 	get_tree().quit()
+
+
+#-------------Score Menu
+func _on_ButtonScores_pressed():
+	$Scores/AnimationScore.play("select_in")
+
+
+func _on_ButtonBack_pressed():
+	$Scores/AnimationScore.play("select_out")
